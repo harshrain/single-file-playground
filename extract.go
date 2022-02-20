1ae7728b6745d368050d321d665f1b1c255f4d29
Harsh <harsh@rain.bh>
2022-02-21 00:07:46 +0400

    Extracted time from retrieved data from papertrail

--- extract.go
+++ extract.go
@@ -3,48 +3,42 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 )
 
 func main() {
-	// m, _ := regexp.Compile("[0-9]+.[0-9]+")
-	// fmt.Println(m.FindString("The time is 8.0978654s"))
 
-	// cmd := exec.Command("papertrail", "'error tap gateway timed out'", "--min-time", "'2022-02-10 00:00:00'", "--max-time", "'2022-02-16 00:00:00'", ">", "testPapertrail.txt")
-	// fmt.Println(cmd.Args)
-	// fmt.Println(cmd.Run())
-
-	// cmd := exec.Command("/Users/harsh/rain", "user:get", "-e", "harsh@rain.bh", "-c", "/Users/harsh/production.toml")
-
-	// cmd := exec.Command("rain", "user:get", "-e", "harsh@rain.bh", "-c", "/Users/harsh/production.toml")
-
-	//Note : String argument like email address will go withoud additional quotes
-
-	cmd := exec.Command("./rain", "user:get", "-e", "harsh@rain.bh", "-c", "production.toml")
-
-	//Use cmd.Dir to target a directory for which this command will be executed
-	cmd.Dir = "/Users/harsh/"
-
+	//Command with file write is not working. Wirkaroung - use os library and write it to file using write function
+	// cmd := exec.Command("papertrail", "error tap gateway timed out", "--min-time", "2022-02-10 00:00:00", "--max-time", "2022-02-16 00:00:00", ">", "testPapertrail.txt")
+	cmd := exec.Command("papertrail", "error tap gateway timed out", "--min-time", "2022-02-10 00:00:00", "--max-time", "2022-02-16 00:00:00")
 	fmt.Println(cmd.Args)
 
-	// var out bytes.Buffer
-	// var stderr bytes.Buffer
-	// cmd.Stdout = &out
-	// cmd.Stderr = &stderr
-	// err := cmd.Run()
-
-	// if err != nil {
-	// 	fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-	// 	return
+	//If you want to store retrieved data in file then un-comment following code to create file
+	// file, er := os.Create("sample.txt")
+	// if er != nil {
+	// 	panic(er)
 	// }
-	// fmt.Println("Result: " + out.String())
+	// defer file.Close()
 
-	//Get the output of the command
 	out, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println("Something went wrong : ", err)
 	}
 
-	fmt.Println(string(out))
+	//Un-comment following code to write retrieved data into the created file
+	// _, errr := file.Write(out)
+	// if err != nil {
+	// 	fmt.Println("Something went wrong : ", errr)
+	// }
 
+	retrievedData := string(out)
+
+	//Setting a pattern to be searched in retrieved data
+	m, _ := regexp.Compile("[0-9]+\\.[0-9]+")
+
+	result := m.FindAllString(retrievedData, -1)
+
+	if result != nil {
+		fmt.Println(result)
+	}
 }
