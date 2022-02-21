62e7995d71ec99439c422566d6d39eebbc08cb2d
Harsh <harsh@rain.bh>
2022-02-22 00:16:20 +0400

    v0.1

--- extract.go
+++ extract.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
+	"sort"
+	"strconv"
 )
 
 func main() {
 
 	//Command with file write is not working. Wirkaroung - use os library and write it to file using write function
 	// cmd := exec.Command("papertrail", "error tap gateway timed out", "--min-time", "2022-02-10 00:00:00", "--max-time", "2022-02-16 00:00:00", ">", "testPapertrail.txt")
-	cmd := exec.Command("papertrail", "error tap gateway timed out", "--min-time", "2022-02-10 00:00:00", "--max-time", "2022-02-16 00:00:00")
+	cmd := exec.Command("papertrail", "'received response for https://api.tap.company/v2/charges'", "--min-time", "2022-02-19 00:00:00", "--max-time", "2022-02-20 00:00:00")
 	fmt.Println(cmd.Args)
 
 	//If you want to store retrieved data in file then un-comment following code to create file
@@ -36,9 +38,36 @@ func main() {
 	//Setting a pattern to be searched in retrieved data
 	m, _ := regexp.Compile("[0-9]+\\.[0-9]+")
 
-	result := m.FindAllString(retrievedData, -1)
+	timesInString := m.FindAllString(retrievedData, -1)
 
-	if result != nil {
-		fmt.Println(result)
+	fmt.Println("Fist : ", timesInString[0])
+	fmt.Println("second : ", timesInString[1])
+	fmt.Println("third : ", timesInString[2])
+	fmt.Println("fourth : ", timesInString[3])
+
+	fmt.Println("Length of timesInString : ", len(timesInString))
+
+	if timesInString != nil {
+
+		fmt.Println("Total : ", len(timesInString))
+		// sort.(result)
+		var timesInFloat = []float64{}
+		sum := 0.0
+		for _, timeInString := range timesInString {
+			timeInFloat, err := strconv.ParseFloat(timeInString, 64)
+			if err != nil {
+				fmt.Println("Something went wrong! Failed to covert string into int")
+			}
+			sum += timeInFloat
+			timesInFloat = append(timesInFloat, timeInFloat)
+		}
+
+		sort.Float64s(timesInFloat)
+		fmt.Println("Minimum : ", timesInFloat[0])
+		fmt.Println("Maximum : ", timesInFloat[len(timesInFloat)-1])
+		fmt.Println("Avg : ", sum/float64(len(timesInFloat)))
+		temp := float64((95 * len(timesInFloat)) / 100)
+		fmt.Println("95th Percentile : ", timesInFloat[int(temp)])
+		// fmt.Println(result)
 	}
 }
